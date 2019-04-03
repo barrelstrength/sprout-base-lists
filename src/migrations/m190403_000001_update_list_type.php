61df7660d96c3270c272ed5dbd95eb36096817d9
Ben Parizek <ben@barrelstrengthdesign.com>
2019-04-03 11:53:34 -0400

    Adds update list type migration

---
+++ src/migrations/m190403_000001_update_list_type.php
@@ -0,0 +1,43 @@
+<?php /** @noinspection ClassConstantCanBeUsedInspection */
+
+namespace barrelstrength\sproutbaselists\migrations;
+
+use craft\db\Migration;
+
+/**
+ * Class m190327_000001_update_list_type
+ *
+ * @package barrelstrength\sproutbaselists\migrations
+ */
+class m190327_000001_update_list_type extends Migration
+{
+    /**
+     * @return bool
+     */
+    public function safeUp(): bool
+    {
+        $listClasses = [
+            0 => [
+                'oldType' => 'barrelstrength\sproutlists\listtypes\SubscriberListType',
+                'newType' => 'barrelstrength\sproutbaselists\listtypes\MailingList'
+            ]
+        ];
+
+        foreach ($listClasses as $class) {
+            $this->update('{{%sproutlists_lists}}', [
+                'type' => $class['newType']
+            ], ['type' => $class['oldType']], [], false);
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m190327_000001_update_list_type cannot be reverted.\n";
+        return false;
+    }
+}
